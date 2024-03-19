812b785c5049b4377b34e7e57ce9389d1f3573b1
YUMINYUMINLEE <94200623+YUMINYUMINLEE@users.noreply.github.com>
2024-03-19 21:50:22 +0900

    Title: 모이자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240319/모이자/gather.cpp
+++ 240319/모이자/gather.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <algorithm>
 #include <vector>
+#include <climits>
 using namespace std;
 
 int main() {
@@ -12,15 +13,15 @@ for(int i=0;i<n;i++){
     cin>>arr[i];
 }
 
-int max_sum=0;
+int min_sum=INT_MAX;
 for(int i=0;i<n;i++){
     int home=arr[i];
     int sum=0;
-    for(int j=0;j<n-1;j++){
-        sum+=abs(arr[j]-arr[i]);
+    for(int j=0;j<n;j++){
+        sum+=abs(i-j)*arr[j];
     }
-    max_sum=max(max_sum,sum);
+    min_sum=min(min_sum,sum);
 }
 
-cout<<max_sum;
+cout<<min_sum;
 }
