02b231e3bc4a3e06404ca8aacf5e61b2b9c7d39c
YUMINYUMINLEE <94200623+YUMINYUMINLEE@users.noreply.github.com>
2024-03-20 17:55:22 +0900

    Title: 야바위, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240320/야바위/ya-rock.cpp
+++ 240320/야바위/ya-rock.cpp
@@ -14,24 +14,20 @@ int main() {
     }
 
     int max_score = 0;
-    // 모든 가능한 시작 위치에 대해 시뮬레이션 실행
     for (int start = 1; start <= 3; ++start) {
         int score = 0;
-        int position = start; // 조약돌이 현재 있는 컵의 위치
+        int position = start;
         for (int i = 0; i < N; ++i) {
             int a = swaps[i * 3], b = swaps[i * 3 + 1], c = swaps[i * 3 + 2];
-            // 교환 수행
             if (position == a) {
                 position = b;
             } else if (position == b) {
                 position = a;
             }
-            // 점수 계산
             if (position == c) {
                 ++score;
             }
         }
-        // 최대 점수 업데이트
         max_score = max(max_score, score);
     }
 
