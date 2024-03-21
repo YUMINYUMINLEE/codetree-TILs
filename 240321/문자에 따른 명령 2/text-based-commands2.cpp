082da177e937f149f4cccff217b7ad54accfccc0
YUMINYUMINLEE <94200623+YUMINYUMINLEE@users.noreply.github.com>
2024-03-21 22:39:51 +0900

    Title: 문자에 따른 명령 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240321/문자에 따른 명령 2/text-based-commands2.cpp
+++ 240321/문자에 따른 명령 2/text-based-commands2.cpp
@@ -3,29 +3,23 @@
 using namespace std;
 int main() {
     string s;
-    cin>>s;
+    cin >> s;
 
-    int x=0,y=0,dir=0;
+    int x = 0, y = 0, dir = 3; // dir를 0(북쪽)으로 설정합니다.
 
-    int dx[4]={1,0,-1,0},dy[4]={0,-1,0,1};
+    // 동, 남, 서, 북 순으로 dx, dy를 설정합니다.
+    int dx[4] = {1, 0, -1, 0}, dy[4] = {0, -1, 0, 1};
 
-    for(int i=0;i<s.size();i++){
-        if(s[i]=='L'){
-            if(dir==0){
-                dir=3;
-            }
-            dir-=1;
-        }
-        else if(s[i]=='R'){
-            if(dir==3){
-                dir=0;
-            }
-            dir+=1;
-        }
-        else if(s[i]=='F'){
-            x+=dx[dir];
-            y+=dy[dir];
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == 'L') {
+            dir = (dir + 3) % 4; 
+        } else if (s[i] == 'R') {
+            dir = (dir + 1) % 4; 
+        } else if (s[i] == 'F') {
+            x += dx[dir]; 
+            y += dy[dir];
         }
     }
-    cout<<x<<" "<<y;
+    cout << x << " " << y;
+    return 0;
 }
