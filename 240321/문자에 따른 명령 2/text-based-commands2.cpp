e13700016b1ac327a1a02a6d863f1e92e893ca28
YUMINYUMINLEE <94200623+YUMINYUMINLEE@users.noreply.github.com>
2024-03-21 22:40:04 +0900

    Title: 문자에 따른 명령 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240321/문자에 따른 명령 2/text-based-commands2.cpp
+++ 240321/문자에 따른 명령 2/text-based-commands2.cpp
@@ -5,9 +5,9 @@ int main() {
     string s;
     cin >> s;
 
-    int x = 0, y = 0, dir = 3; // dir를 0(북쪽)으로 설정합니다.
+    int x = 0, y = 0, dir = 3;
 
-    // 동, 남, 서, 북 순으로 dx, dy를 설정합니다.
+
     int dx[4] = {1, 0, -1, 0}, dy[4] = {0, -1, 0, 1};
 
     for (int i = 0; i < s.size(); i++) {
