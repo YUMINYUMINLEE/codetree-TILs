96b4e11d7e03155470cacfdd962960c8c566727e
YUMINYUMINLEE <94200623+YUMINYUMINLEE@users.noreply.github.com>
2024-03-21 21:21:13 +0900

    Title: n개 중에 m개 뽑기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240321/n개 중에 m개 뽑기/n-choose-m.cpp
+++ 240321/n개 중에 m개 뽑기/n-choose-m.cpp
@@ -3,39 +3,31 @@
 
 using namespace std;
 
-int n, m;
-vector<int> combination;
+vector<int> answer;
 
-// 방문한 원소들을 출력해줍니다.
-void PrintCombination() {
-    for(int i = 0; i < combination.size(); i++)
-        cout << combination[i] << " ";
-    cout << endl;
+void Print() {
+    for (int num : answer) {
+        cout << num << " ";
+    }
+    cout << "\n";
 }
 
-// 지금까지 뽑은 갯수와 뽑을지 말지를 결정할 숫자를 인자로 받습니다. 
-void FindCombination(int curr_num, int cnt) {
-
-    // n개의 숫자를 모두 탐색했으면 더 이상 탐색하지 않습니다.
-    if(curr_num == n+1) {
-        // 탐색하는 과정에서 m개의 숫자를 뽑은 경우 답을 출력해줍니다.
-        if(cnt == m)
-            PrintCombination();
+void Choose(int start, int m, int depth) {
+    if (depth == m) {
+        Print();
         return;
     }
 
-    // curr_num에 해당하는 숫자를 사용했을 때의 경우를 탐색합니다.
-    combination.push_back(curr_num);
-    FindCombination(curr_num + 1, cnt + 1);
-    combination.pop_back();
-
-    // curr_num에 해당하는 숫자를 사용하지 않았을 때의 경우를 탐색합니다.
-    FindCombination(curr_num + 1, cnt);
+    for (int i = start; i <= n; ++i) {
+        answer.push_back(i);
+        Choose(i + 1, m, depth + 1); 
+        answer.pop_back();
+    }
 }
 
 int main() {
+    int m;
     cin >> n >> m;
-
-    FindCombination(1, 0);
+    Choose(1, m, 0); 
     return 0;
 }
