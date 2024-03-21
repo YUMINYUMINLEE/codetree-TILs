b5cd9462144be345425b1fe2ca6331feb26dacb1
YUMINYUMINLEE <94200623+YUMINYUMINLEE@users.noreply.github.com>
2024-03-21 21:19:54 +0900

    Title: n개 중에 m개 뽑기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240321/n개 중에 m개 뽑기/n-choose-m.cpp
+++ 240321/n개 중에 m개 뽑기/n-choose-m.cpp
@@ -2,48 +2,40 @@
 #include <vector>
 
 using namespace std;
-int n;
-vector<int> answer;
 
-void Print(){
-    for(int i=0;i<answer.size();i++){
-        cout<<answer[i]<<" ";
-    }
-    cout<<"\n";
+int n, m;
+vector<int> combination;
+
+// 방문한 원소들을 출력해줍니다.
+void PrintCombination() {
+    for(int i = 0; i < combination.size(); i++)
+        cout << combination[i] << " ";
+    cout << endl;
 }
 
-void Choose(int curr_num,int m,vector<int>& arr){
-    if(curr_num==m+1){
-    
-        for(int i=0;i<answer.size()-1;i++){
-            if(answer[i]>=answer[i+1]){
-                return;
-            }
-        }
-                                                                           
-        Print();
+// 지금까지 뽑은 갯수와 뽑을지 말지를 결정할 숫자를 인자로 받습니다. 
+void FindCombination(int curr_num, int cnt) {
+
+    // n개의 숫자를 모두 탐색했으면 더 이상 탐색하지 않습니다.
+    if(curr_num == n+1) {
+        // 탐색하는 과정에서 m개의 숫자를 뽑은 경우 답을 출력해줍니다.
+        if(cnt == m)
+            PrintCombination();
         return;
     }
 
-    for(int i=1;i<=n;i++){
-        if(arr[i-1]==1){
-            continue;
-        }
-        else{
-            answer.push_back(i);
-            arr[i-1]=1;
-        }
-        Choose(curr_num+1,m,arr);
-        answer.pop_back();
-        arr[i-1]=0;
+    // curr_num에 해당하는 숫자를 사용했을 때의 경우를 탐색합니다.
+    combination.push_back(curr_num);
+    FindCombination(curr_num + 1, cnt + 1);
+    combination.pop_back();
 
-    }
+    // curr_num에 해당하는 숫자를 사용하지 않았을 때의 경우를 탐색합니다.
+    FindCombination(curr_num + 1, cnt);
 }
 
 int main() {
-    int m;
-    cin>>n>>m;
-    vector<int> arr(n);
-    Choose(1,m,arr);
+    cin >> n >> m;
+
+    FindCombination(1, 0);
     return 0;
 }
