308e232db14005865f6149fbfeaf6d94582d83d1
YUMINYUMINLEE <94200623+YUMINYUMINLEE@users.noreply.github.com>
2024-03-21 22:25:29 +0900

    Title: 문자에 따른 명령 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240321/문자에 따른 명령 2/text-based-commands2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    string s;
+    cin>>s;
+
+    int x=0,y=0,dir=3;
+
+    int dx[4]={1,0,-1,0},dy[4]={0,1,0,1};
+    int direction[4]={0,1,2,3};//동,남,서,북
+
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='L'){
+            if(dir==0){
+                dir=3;
+            }
+            dir-=1;
+        }
+        else if(s[i]=='R'){
+            if(dir==3){
+                dir=0;
+            }
+            dir+=1;
+        }
+        else if(s[i]=='F'){
+            x+=dx[dir];
+            y+=dy[dir];
+        }
+    }
+    cout<<x<<" "<<y;
+}
