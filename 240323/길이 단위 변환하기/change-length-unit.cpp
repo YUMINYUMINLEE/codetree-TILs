84116e7f1b624288f238d1f8ec298765f7ae8050
YUMINYUMINLEE <94200623+YUMINYUMINLEE@users.noreply.github.com>
2024-03-23 19:35:15 +0900

    Title: 길이 단위 변환하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240323/길이 단위 변환하기/change-length-unit.cpp
+++ 240323/길이 단위 변환하기/change-length-unit.cpp
@@ -4,6 +4,9 @@ int main() {
     double one_fit=30.48;
     double one_mi=160934;
 
-    std::cout<<"9.2ft = "<<9.2*one_fit;<<std::endl;
-    std::cout<<"1.3mi = "<<1.3*one_mi;
+    std::cout<<std::fixed;
+    std::cout.precision(1);
+
+    std::cout<<"9.2ft = "<<9.2*one_fit<<"cm"<<std::endl;
+    std::cout<<"1.3mi = "<<1.3*one_mi<<"cm";
 }
