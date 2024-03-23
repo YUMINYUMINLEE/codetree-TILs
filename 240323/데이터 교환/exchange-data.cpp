9ecdf112c59c963f44a99312c648efc1997bac09
YUMINYUMINLEE <94200623+YUMINYUMINLEE@users.noreply.github.com>
2024-03-23 19:46:24 +0900

    Title: 데이터 교환, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 240323/데이터 교환/exchange-data.cpp
+++ 240323/데이터 교환/exchange-data.cpp
@@ -2,13 +2,14 @@
 
 int main() {
     int a=5,b=6,c=7;
-    int temp1,temp2;
+    int temp;
 
-    temp1=b;
-    temp2=c;
-    b=a;
-    c=temp1;
-    a=temp2;
+    temp=a;
+    a=c;
+    c=b;
+    b=temp;
+
+    
 
     std::cout<<a<<"\n"<<b<<"\n"<<c;
 
