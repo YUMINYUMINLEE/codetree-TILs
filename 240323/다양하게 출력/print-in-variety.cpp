43769ddb1a1e7166bd2c5a9be05d7513eb32971a
YUMINYUMINLEE <94200623+YUMINYUMINLEE@users.noreply.github.com>
2024-03-23 19:16:49 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240323/다양하게 출력/print-in-variety.cpp
+++ 240323/다양하게 출력/print-in-variety.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
 int main() {
-    std::cout<<"Total days in Year"<<"\n"<<365<<"\n"<<"Circumference rate"<<"\n"<<"3.1415926535";
     
+    std::cout<<std::fixed;
+    std::cout.precision(10);
+    std::cout<<"Total days in Year"<<"\n"<<365<<"\n"<<"Circumference rate"<<"\n"<<3.1415926535;
     return 0;
 }
