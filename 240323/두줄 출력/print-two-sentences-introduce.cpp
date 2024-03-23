6eaf1f96f26b22c7ea5f57c0d21be9ddfa4bdfb4
YUMINYUMINLEE <94200623+YUMINYUMINLEE@users.noreply.github.com>
2024-03-23 19:12:55 +0900

    Title: 두줄 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240323/두줄 출력/print-two-sentences-introduce.cpp
+++ 240323/두줄 출력/print-two-sentences-introduce.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout<<"Hello students!"<<"\n"
-    std::cout<<"Welcome to LeebrosCode"
+    std::cout<<"Hello students!"<<"\n";
+    std::cout<<"Welcome to LeebrosCode";
 }
