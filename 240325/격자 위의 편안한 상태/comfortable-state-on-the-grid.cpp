1f182b233d5895596ba4077f12c9e3d151f2f7ae
YUMINYUMINLEE <94200623+YUMINYUMINLEE@users.noreply.github.com>
2024-03-25 20:52:10 +0900

    Title: 격자 위의 편안한 상태, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240325/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+bool cf[100][100];
+int main() {
+
+    int dx[4]={-1,0,1,0},dy[4]={0,1,0,-1}; //위,오른쪽,아래,왼쪽;
+                                       //0,    1  , 2   ,3
+
+
+    int N,M;
+    cin>>N>>M;
+    bool arr[100][100];
+
+    while(M--){
+        int x,y;
+        cin>>x>>y;
+        arr[x][y]=true;
+
+        int cnt=0;
+        for(int i=0;i<4;i++){
+            if(arr[x+dx[i]][y+dy[i]]==true)
+                cnt++;
+        }
+        if(cnt==3){
+            cout<<1<<"\n";
+        }
+        else
+            cout<<0<<"\n";
+       
+
+
+
+
+    }
+
+    
+
+}
