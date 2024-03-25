0f7a39eea695d1b6744761bd1b2478ad43401517
YUMINYUMINLEE <94200623+YUMINYUMINLEE@users.noreply.github.com>
2024-03-25 23:39:20 +0900

    Title: 거울에 레이저 쏘기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240325/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
+++ 240325/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
@@ -2,18 +2,23 @@
 #include <vector>
 using namespace std;
 int n;
+//k의 시작 위치
+bool InRange(int x,int y){
+    return(x>=0&&x<n&&y>=0&&y<n);
+
+}
 int StartDirection(int k){
     if(k>=1&&k<=n){
-        return 2
+        return 2;
     }
-    if(k>=n+1&&k<=2n){
-        return 1
+    if(k>=n+1&&k<=2*n){
+        return 1;
     }
-    if(k>=2n+1&&k<=3n){
-        return 0
+    if(k>=2*n+1&&k<=3*n){
+        return 0;
     }
-    if(k>=3n+1&&k<=4n){
-        return 3
+    if(k>=3*n+1&&k<=4*n){
+        return 3;
     }
 }
 
@@ -32,8 +37,144 @@ int main() {
         }
     }
     cin>>k;
+    int dir;
+    int kx,ky;
+    int cnt=0;
 
-    
+    dir=StartDirection(k);
+    int offset=k%n==0?n:k%n;
+    while(true){
+        if(dir==0){
+            kx=n-1,ky=n-offset-1;
+            if(arr[kx][ky]=='\\'){
+                dir=3;
+                kx+=dx[dir],ky+=dy[dir];
+                cnt++;
+                if(!InRange(kx,ky)){
+                    cout<<cnt;
+                    break;
+                }
+                dir=1;  
+            }
+            if(arr[kx][ky]=='/'){
+                dir=1;
+                kx+=dx[dir],ky+=dy[dir];
+                cnt++;
+                if(!InRange(kx,ky)){
+                    cout<<cnt;
+                    break;
+                }
+                dir=3;
+            }
+
+        }
+        if(dir==1){
+            kx=offset-1,ky=n-1;
+            if(arr[kx][ky]=='\\'){
+                dir=2;
+                kx+=dx[dir],ky+=dy[dir];
+                cnt++;
+                if(!InRange(kx,ky)){
+                    cout<<cnt;
+                    break;
+                }
+                dir=0;  
+            }
+            if(arr[kx][ky]=='/'){
+                dir=0;
+                kx+=dx[dir],ky+=dy[dir];
+                cnt++;
+                if(!InRange(kx,ky)){
+                    cout<<cnt;
+                    break;
+                }
+                dir=2;
+            }
+        }
+        if(dir==2){
+            kx=0,ky=offset-1;
+            if(arr[kx][ky]=='\\'){
+                dir=1;
+                kx+=dx[dir],ky+=dy[dir];
+                cnt++;
+                if(!InRange(kx,ky)){
+                    cout<<cnt;
+                    break;
+                }
+                dir=3;  
+            }
+            if(arr[kx][ky]=='/'){
+                dir=3;
+                kx+=dx[dir],ky+=dy[dir];
+                cnt++;
+                if(!InRange(kx,ky)){
+                    cout<<cnt;
+                    break;
+                }
+                dir=1;
+            }
+        }
+        if(dir==3){
+            kx=n-offset-1,ky=0;
+            if(arr[kx][ky]=='\\'){
+                dir=0;
+                kx+=dx[dir],ky+=dy[dir];
+                cnt++;
+                if(!InRange(kx,ky)){
+                    cout<<cnt;
+                    break;
+                }
+                dir=2;  
+            }
+            if(arr[kx][ky]=='/'){
+                dir=2;
+                kx+=dx[dir],ky+=dy[dir];
+                cnt++;
+                if(!InRange(kx,ky)){
+                    cout<<cnt;
+                    break;
+                }
+                dir=0;
+            }
+        }
+    }
+
+
+
+   
+
+    //첫 k인덱스를 잡고
+    //StartDirection으로 방향을 잡고,
+    // 모양에 따라서 방향을 설정해주고 인덱스를 갱신,cnt++해준뒤
+    //격자 범위에서 벗어나면 cnt출력(마지막에서 cnt는 ++된다.)
+
+
+    /*
+    2번 시작(첫 k인덱스를 잡고)
+    \=>위-오른 /cnt++;
+    \=>왼-아래(다음칸 가면 방향이 바뀜); /cnt++;
+    \=>위-오른 인데 범위 체크에서 격자 밖이므로 cnt++ break;
+    출력 cnt;
+    */
+    /*
+    5번 시작
+    \=>오-위 k++/cnt++;
+    \=>아래-왼(다음칸 가면 방향이 바뀜); k++/cnt++;
+    \=>오-위 k++인데 범위 체크에서 격자 밖이므로 cnt++ break;
+    출력 cnt;
+    */
+    /*
+    10번 시작
+    /=>왼-위 k++/cnt++;
+    \=>아래-왼(다음칸 가면 방향이 바뀜); k++범위밖/cnt++;
+    출력 cnt;
+    */
+        /*
+    9번 시작
+    /=>아래-오 k++/cnt++;
+    \=>왼-아래(다음칸 가면 방향이 바뀜); k++범위밖/cnt++;
+    출력 cnt;
+    */
 
 
 }
