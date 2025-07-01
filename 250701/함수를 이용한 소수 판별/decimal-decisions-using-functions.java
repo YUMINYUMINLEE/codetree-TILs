068654d3d161390c453f46e71876e1805c9464aa
YUMINYUMINLEE <94200623+YUMINYUMINLEE@users.noreply.github.com>
2025-07-01 22:20:05 +0900

    Title: 함수를 이용한 소수 판별, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

--- 250701/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
+++ 250701/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -12,7 +12,7 @@ public class Main {
         if(num%2==0){
             return false;
         }
-        for(int i=3;i<=Math.sqrt(num);i++){
+        for(int i=3;i<=Math.sqrt(num);i+=2){
             if(num%i==0){
                 return false;
             }
